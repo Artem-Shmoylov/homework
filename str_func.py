dda7b7f7da12072f7c83db44eb37ca30d452aff7
Artem Shmoylov <artemshmoylov123@gmail.com>
2023-05-16 21:18:44 +0300

    add func str_title and docstrint to str_funcs

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_funcs(line):
+    """возрашает строку в верхнем регистре"""
     return line.upper()
+
+def str_title(line):
+    """возрашает строку со всеми залавными буквами"""
+    return line.title()
